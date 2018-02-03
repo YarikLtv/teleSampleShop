76a02e0fecadfa0f14bd18013d844035e2cd12b1
yong hao <yonghao.choo@gmail.com>
2018-02-04 00:31:31 +0800

    added feature: list products

--- index.py
+++ index.py
@@ -19,14 +19,17 @@ dispatcher.add_handler(start_handler)
 
 #list function
 def list(bot, update):
+    # using urllib3 to send multiple photos to user
     http = urllib3.PoolManager()
-    urls = ['https://vignette.wikia.nocookie.net/adventuretimewithfinnandjake/images/e/e4/At_100x100_iconjake.jpg', 'https://vignette.wikia.nocookie.net/adventuretimewithfinnandjake/images/2/2c/AT_Icons_100x100_Finn.jpg']
+    urls = ['https://burst.shopifycdn.com/photos/choker-with-triangle_925x.jpg', 'https://burst.shopifycdn.com/photos/blue-gemstone-pendant_925x.jpg']
     itemInUrls = int(len(urls))
     captions = ['product1', 'product2']
     iterations = 0
     while iterations < itemInUrls:
         bot.send_photo(chat_id=update.message.chat_id, photo=urls[iterations], caption=captions[iterations])
         iterations += 1
+    # sending additional info
+    bot.send_message(chat_id=update.message.chat_id, text='Use: \n /product1 - for more information on product 1 \n /product2 - for more informaitno on product 2')
 #list handler
 list_handler = CommandHandler('list', list)
 dispatcher.add_handler(list_handler)
@@ -34,13 +37,13 @@ dispatcher.add_handler(list_handler)
 #product1 function
 def product1(bot, update):
     bot.send_invoice(chat_id=update.message.chat_id,
-    title='Product 1',
-    description='This is product1',
+    title='Choker',
+    description='This is a sample choker',
     payload='payload',
     start_parameter='start_parameter',
     currency='SGD',
-    prices=[LabeledPrice('Product1', 1000), LabeledPrice('Product1 tax', (100))],
-    photo_url='https://vignette.wikia.nocookie.net/adventuretimewithfinnandjake/images/e/e4/At_100x100_iconjake.jpg',
+    prices=[LabeledPrice('Product1', 1200), LabeledPrice('Product1 tax', (100))],
+    photo_url='https://burst.shopifycdn.com/photos/choker-with-triangle_925x.jpg',
     # need_name=True,
     # need_email=True,
     # send_email_to_provider=True,
